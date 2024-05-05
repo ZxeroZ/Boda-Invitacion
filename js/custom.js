29961552c23c4f4acf22da40e63c6cb5ca34f45e
ZxeroZ <91742084+ZxeroZ@users.noreply.github.com>
2024-05-04 19:20:28 -0500

    First-Version
    -Images
    -Contact
    -Responsive

---
+++ js/custom.js
@@ -0,0 +1,110 @@
+(function($) {
+    "use strict";
+	
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+    	
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
+	
+	$('.navbar-nav .nav-link').on('click', function() {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+	
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$(document).ready(function() {
+		$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+				}
+			}
+		});
+	});
+	
+	/* ..............................................
+    Smooth Scroll
+    ................................................. */
+	
+	$('a[href*="#"]:not([href="#"])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+		  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html,body').animate({
+				  scrollTop: target.offset().top - 65,
+				  }, 1000);
+				  return false;
+			  }
+		}
+	});
+	
+}(jQuery));
+
+
+
+// Obtener la fecha objetivo (25 de mayo)
+const fechaObjetivo = new Date('2024-05-25T00:00:00');
+
+function actualizarContador() {
+  const ahora = new Date().getTime();
+  const diferencia = fechaObjetivo - ahora;
+
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+  document.getElementById('dias').innerText = dias + 'd';
+  document.getElementById('horas').innerText = horas + 'h';
+  document.getElementById('minutos').innerText = minutos + 'm';
+  document.getElementById('segundos').innerText = segundos + 's';
+
+  if (diferencia < 0) {
+    clearInterval(contadorInterval);
+    document.querySelector('.contador').innerHTML = '<div>¡Ya ha pasado!</div>';
+  }
+}
+
+// Actualizar contador cada segundo
+const contadorInterval = setInterval(actualizarContador, 1000);
+
+// Actualizar contador al cargar la página
+actualizarContador();
